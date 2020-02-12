075904c6ced5fa8287a663a45b3a1dfb7fa9ee81
Joseph <josephcho@algorithmicmind.com>
2020-02-11 19:44:49 -0500

    passed prop from parent component to child and added onclick alert

--- src/index.js
+++ src/index.js
@@ -3,14 +3,23 @@ import ReactDOM from "react-dom";
 import "./index.css";
 
 class Square extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: null,
+    };
+    }
+  
   render() {
-    return <button className="square"> {/* TODO */} </button>;
+    return (
+      <button className="square" onClick={function() {alert("click");}}> {this.props.value} </button>
+    );
   }
 }
 
 class Board extends React.Component {
   renderSquare(i) {
-    return <Square />;
+    return <Square value={i} />;
   }
 
   render() {
